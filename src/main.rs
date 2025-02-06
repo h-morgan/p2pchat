8cd5fe36567e2169c7649644aafe7cc9347b0916
Haley Morgan <haleymorgan3264@gmail.com>
2025-02-05 21:37:21 -0500

    implement basic gossip functionality

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 use anyhow::Result;
-use iroh::{Endpoint, SecretKey};
+use iroh::protocol::Router;
+use iroh::Endpoint;
+use iroh_gossip::net::Gossip;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Generate a secret key. This is the source of
-    // identity for your node. If you want to have
-    // the same identity each time you open the app,
-    // you would need to store and load it each time.
-    let secret_key = SecretKey::generate(rand::rngs::OsRng);
-
-    // Create an endpoint.
+    // We've removed the `SecretKey::generate` method.
+    // The `Endpoint` will generate a `SecretKey` for
+    // you under the hood if you don't supply one.
     let endpoint = Endpoint::builder()
-        // Pass in your secret key. If you don't pass
-        // in a secret key a new one will be generated
-        // for you each time.
-        .secret_key(secret_key)
-        // Enable n0 discovery. This allows you to 
-        // dial by `NodeId`, and allows you to be
-        // dialed by `NodeId`.
         .discovery_n0()
-        // Bind the endpoint to the socket.
         .bind()
         .await?;
 
     println!("> our node id: {}", endpoint.node_id());
+
+    // Build an instance of the gossip protocol
+    // and add a clone of the endpoint we have built.
+    // The gossip protocol will use the endpoint to
+    // make connections.
+    let gossip = Gossip::builder().spawn(endpoint.clone()).await?;
+
+    // The Router is how we manage protocols on top
+    // of the iroh endpoint. It handles all incoming
+    // messages and routes them to the correct
+    // protocol.
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn()
+        .await?;
+
+    // Cleanly shutdown the router.
+    router.shutdown().await?;
 
     Ok(())
 }
