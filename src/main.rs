2aea525055f969e0641350a39d96de4f365c5ac3
Haley Morgan <haleymorgan3264@gmail.com>
2025-02-05 21:46:12 -0500

    add infrastructure to setup a topic subscription

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use anyhow::Result;
 use iroh::protocol::Router;
 use iroh::Endpoint;
-use iroh_gossip::net::Gossip;
+use iroh_gossip::{net::Gossip, proto::TopicId};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // We've removed the `SecretKey::generate` method.
-    // The `Endpoint` will generate a `SecretKey` for
-    // you under the hood if you don't supply one.
-    let endpoint = Endpoint::builder()
-        .discovery_n0()
-        .bind()
-        .await?;
+    let endpoint = Endpoint::builder().discovery_n0().bind().await?;
 
     println!("> our node id: {}", endpoint.node_id());
-
-    // Build an instance of the gossip protocol
-    // and add a clone of the endpoint we have built.
-    // The gossip protocol will use the endpoint to
-    // make connections.
     let gossip = Gossip::builder().spawn(endpoint.clone()).await?;
 
-    // The Router is how we manage protocols on top
-    // of the iroh endpoint. It handles all incoming
-    // messages and routes them to the correct
-    // protocol.
     let router = Router::builder(endpoint.clone())
         .accept(iroh_gossip::ALPN, gossip.clone())
         .spawn()
         .await?;
 
-    // Cleanly shutdown the router.
+    // Create a new topic.
+    let id = TopicId::from_bytes(rand::random());
+    let node_ids = vec![];
+
+    // Subscribe to the topic.
+    // Since the `node_ids` list is empty, we will
+    // subscribe to the topic, but not attempt to
+    // connect to any other nodes.
+    let topic = gossip.subscribe(id, node_ids)?;
+
+    // `split` splits the topic into the `GossipSender`
+    // and `GossipReceiver` portions
+    let (sender, _receiver) = topic.split();
+
+    // Broadcast a messsage to the topic.
+    // Since no one else is apart of this topic,
+    // this message is currently going out to no one.
+    sender.broadcast("sup".into()).await?;
+
     router.shutdown().await?;
 
     Ok(())
