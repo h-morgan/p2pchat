11a499f548295afd15e94d14ff4ef1e58600b773
Haley Morgan <haleymorgan3264@gmail.com>
2025-02-05 21:27:05 -0500

    add secret key generation and connection scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use iroh::{Endpoint, SecretKey};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Generate a secret key. This is the source of
+    // identity for your node. If you want to have
+    // the same identity each time you open the app,
+    // you would need to store and load it each time.
+    let secret_key = SecretKey::generate(rand::rngs::OsRng);
+
+    // Create an endpoint.
+    let endpoint = Endpoint::builder()
+        // Pass in your secret key. If you don't pass
+        // in a secret key a new one will be generated
+        // for you each time.
+        .secret_key(secret_key)
+        // Enable n0 discovery. This allows you to 
+        // dial by `NodeId`, and allows you to be
+        // dialed by `NodeId`.
+        .discovery_n0()
+        // Bind the endpoint to the socket.
+        .bind()
+        .await?;
+
+    println!("> our node id: {}", endpoint.node_id());
+
+    Ok(())
 }
